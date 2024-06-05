ed14f4df30b681b578793bd64d6d9b84d0171e49
Franklin da Silva Batista <frsbatist@gmail.com>
2024-06-05 18:28:13 -0300

    Layout
    Componentização do layout.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,12 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    @vite('resources/css/app.css', 'resources/js/app.js');
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+
+    @vite('resources/css/app.css', 'resources/js/app.js')
 
     <title>Pixel Positions</title>
 </head>
 
-<body class="bg-black text-white">
+<body class="bg-black text-white font-hanken-grotesk">
     <div class="px-10">
 
         <nav class="flex justify-between items-center py-4 border-b border-white/10 font-bold">
@@ -21,10 +26,10 @@
                 </a>
             </div>
             <div class="space-x-6">
-                
+
                 <a href="#">Jobs</a>
                 <a href="#">Carees</a>
-                <a href="#">Slaries</a>
+                <a href="#">Salaries</a>
                 <a href="#">Companies</a>
 
             </div>
