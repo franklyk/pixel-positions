a4b414efff4496c73c6cd4c2f266aff4a00d29ae
Franklin da Silva Batista <frsbatist@gmail.com>
2024-06-22 09:32:38 -0300

    Label
    Otimização

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -4,7 +4,8 @@
             <h1 class="font-bold text-4xl">Let's Find Your Next Job</h1>
 
             <x-forms.form action="/search" class="mt-6">
-                <x-forms.input :label="false" name="q" placeholder="Web Developer..."/>
+                
+                <x-forms.input type="search" :label="false" name="q" placeholder="Web Developer..."/>
 
             </x-forms.form>
 
